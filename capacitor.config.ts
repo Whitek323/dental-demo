c225a9146e1caad97161ba191f51d37c9ab1ef61
Whitek323 <whitek8327@gmail.com>
2025-05-22 18:24:10 +0700

    last commit2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'dental-demo',
+  webDir: 'dist'
+};
+
+export default config;
